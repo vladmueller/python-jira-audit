943d2f681fcd0cee44aa0eccbfb99608211ea901
Vladimir Müller <code@vladmueller.de>
2025-02-18 14:39:59 +0100

    ♻ Move functions related to project resources in own file

--- labels.py
+++ labels.py
@@ -2,23 +2,11 @@ import json
 import os
 import requests
 from dotenv import load_dotenv
-from jira import JIRA
+from projects import build_projects_jql
 from requests.auth import HTTPBasicAuth
 
 load_dotenv()
 JIRA_BASE_URL = os.getenv("JIRA_BASE_URL")
-
-jira = JIRA(JIRA_BASE_URL, basic_auth=(os.getenv("JIRA_EMAIL"), os.getenv("JIRA_API_TOKEN")))
-
-
-def fetch_project_keys():
-    return [project.key for project in jira.projects()]
-
-
-def build_projects_jql():
-    project_keys = fetch_project_keys()
-    jql_string = f"project IN ({', '.join(f'\'{key}\'' for key in project_keys)})"
-    return jql_string
 
 
 def search():
